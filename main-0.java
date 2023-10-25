5a14b5836d691058e4fb4adf6900b5fbdf0129ba
Md. Abu Talha <101306544+md-abutalha@users.noreply.github.com>
2023-10-25 21:57:20 +0600

    Create main-0.java

---
+++ main-0.java
@@ -0,0 +1,14 @@
+package problem_solve;
+
+public class pattern_2 {
+    public static void main(String[] args) {
+        System.out.println("Hello I'm Talha");
+        for (int row = 1; row<=5; row++){
+           for (int col = 1; col<=5; col++){
+               System.out.print(" * ");
+
+           }
+            System.out.println();
+        }
+    }
+}
